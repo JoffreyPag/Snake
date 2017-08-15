fd103599fe445d3c830b4c45cfe4c3cdff54dd53
joffr <joffreypag@gmail.com>
2017-08-15 19:19:26 -0300

    te

--- app/src/main/java/com/example/joffr/snake/MainActivity.java
+++ app/src/main/java/com/example/joffr/snake/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +17,14 @@ public class MainActivity extends AppCompatActivity {
     public void NovoJogo(View v){
         Intent i = new Intent(this, Configuracoes.class);
         startActivity(i);
+        Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show();
 
     }
 
     public void Continuar(View v){
         Intent i = new Intent(this, Configuracoes.class);
         startActivity(i);
+        Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show();
 
     }
     public void Config(View v){
