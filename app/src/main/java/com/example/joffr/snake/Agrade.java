21f3ae41a353c257d8862f421b019e81d13c7a31
joffr <joffreypag@gmail.com>
2017-08-15 22:02:04 -0300

    teste

---
+++ app/src/main/java/com/example/joffr/snake/Agrade.java
@@ -0,0 +1,13 @@
+package com.example.joffr.snake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Agrade extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agrade);
+    }
+}
