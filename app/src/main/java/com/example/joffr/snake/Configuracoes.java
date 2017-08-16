4ae9c57f4dd61a61dbf139fd73381718420b6f7b
joffr <joffreypag@gmail.com>
2017-08-15 21:06:35 -0300

    test

--- app/src/main/java/com/example/joffr/snake/Configuracoes.java
+++ app/src/main/java/com/example/joffr/snake/Configuracoes.java
@@ -1,18 +1,83 @@
 package com.example.joffr.snake;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 
 public class Configuracoes extends AppCompatActivity {
+    int dif;
+    int tamgrid;
+    RadioButton rb1, rb2, rb3, rbp, rbg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
+
+        rb1 = (RadioButton)findViewById(R.id.fac);
+        rb2 = (RadioButton)findViewById(R.id.medio);
+        rb3 = (RadioButton)findViewById(R.id.dificil);
+        rbp = (RadioButton)findViewById(R.id.peq);
+        rbg = (RadioButton)findViewById(R.id.gran);
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.fac:
+                if (checked) {
+                    dif = 1;
+                    rb2.setChecked(false);
+                    rb3.setChecked(false);
+                }
+                    break;
+            case R.id.medio:
+                if (checked) {
+                    dif = 2;
+                    rb1.setChecked(false);
+                    rb3.setChecked(false);
+                }
+                break;
+            case R.id.dificil:
+                if(checked) {
+                    dif = 3;
+                    rb1.setChecked(false);
+                    rb2.setChecked(false);
+                }
+                break;
+            case R.id.peq:
+                if(checked){
+                    tamgrid = 1;
+                    rbg.setChecked(false);
+                }
+                break;
+            case R.id.gran:
+                if(checked){
+                    tamgrid = 2;
+                    rbp.setChecked(false);
+                }
+            break;
+        }
+    }
+
+    public void Alterar(View v){
+        Intent i = new Intent();
+        Bundle b = new Bundle();
+        b.putInt("dificuldade", dif);
+        b.putInt("tamanhograde", tamgrid);
+        i.putExtras(b);
+        setResult(RESULT_OK, i);
+        finish();
     }
 
     public void voltar(View v){
+        setResult(RESULT_CANCELED);
         finish();
     }
 }
--- app/src/main/java/com/example/joffr/snake/MainActivity.java
+++ app/src/main/java/com/example/joffr/snake/MainActivity.java
@@ -1,24 +1,39 @@
 package com.example.joffr.snake;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String SALVASTATUS = "ULTIMOSAVE";
+    private static final String PONTUACAO = "PONTMAX";
+    private static final String DIFICULDADE = "DIF";
+    private static final int CONFS = 11;
+
+    int pontuacao;
+    int dif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences save = getSharedPreferences(SALVASTATUS, MODE_PRIVATE);
+
+        pontuacao = save.getInt(PONTUACAO, 0);
+        dif = save.getInt(DIFICULDADE, 2);
     }
 
     public void NovoJogo(View v){
         Intent i = new Intent(this, Configuracoes.class);
+        Bundle b = new Bundle();
+        b.putInt("pontMax", pontuacao);
+        b.putInt("dificuldade", dif);
+        i.putExtras(b);
         startActivity(i);
-        Toast.makeText(this, "aaa", Toast.LENGTH_SHORT).show();
-
     }
 
     public void Continuar(View v){
@@ -29,6 +44,15 @@ public class MainActivity extends AppCompatActivity {
     }
     public void Config(View v){
         Intent i = new Intent(this, Configuracoes.class);
-        startActivity(i);
+        startActivityForResult(i, CONFS);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode ==  CONFS){
+            if(resultCode == RESULT_OK){
+                dif = data.getIntExtra("dificuldade");
+            }
+        }
     }
 }
